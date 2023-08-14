d328a254eda2061f5a7e21ca68ac66d00158a8a8
Viktor <121170729+ViktorPospelov@users.noreply.github.com>
2023-08-14 17:50:27 +0500

    fix

---
+++ Assets/cat.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cat : MonoBehaviour
+{
+    [SerializeField] private GameObject cry;
+    [SerializeField] private Collider2D collider2D;
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.otherCollider == collider2D)
+        {
+            cry.SetActive(true);
+        }
+    }
+}
